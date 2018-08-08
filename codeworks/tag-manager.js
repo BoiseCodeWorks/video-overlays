1bb6f7c6dca9e706c2fad9e32d84c4102bce5b48
Jake Overall <joverall22@gmail.com>
2018-08-08 00:42:49 -0600

    adds minmalistic overlay

--- codeworks/tag-manager.js
+++ codeworks/tag-manager.js
@@ -1,32 +1,51 @@
-(function(){
+(function () {
   let img = document.getElementById('img');
-  let state = {in: false, pos: -1}
-  let cords = [{in: {x: 1, y: 1}, outs:[{x: -30, y: 1}, {x: 1, y: -35}]}, {in: {x:95, y: 60}, outs:[{x: 150, y: 60}, {x:95, y: 80}]}, {in: {x:1, y: 60}, outs:[{x: -50, y: 60}, {x:1, y: 80}]}]
-  
-  function transition(){
-    if(state.in){
+  setInstructor()
+
+  let ul = { in: { x: 95, y: 60 }, outs: [{ x: 150, y: 60 }, { x: 95, y: 80 }] }
+  let cords = [{ in: { x: 1, y: 1 }, outs: [{ x: -50, y: 1 }, { x: 1, y: -35 }] }, { in: { x: 1, y: 60 }, outs: [{ x: -50, y: 60 }, { x: 1, y: 80 }] }]
+  let state = { in: false, pos: 0 }
+  let t = 60000
+  function transition() {
+    console.log(state)
+    if (state.in) {
       state.in = false
       let n = cords[state.pos].outs[Math.floor(Math.random() * cords[state.pos].outs.length)]
-      setTimeout(transition, 6000 * 3)
-      return setImg(n)
+      setImg(n)
+      setTimeout(transition, t * 3)
+      return
     }
     state.in = true
     changePos()
-    setImg(cords[state.pos])
-    setTimeout(transition, 6000 * 8)
+    setImg(cords[state.pos].outs[0])
+    setTimeout(() => {
+      setImg(cords[state.pos].in)
+      setTimeout(transition, t * 10)
+    }, 1000)
   }
-  
-  function changePos(){
-    let n = Math.floor(Math.random() * cords.length)
+
+  function changePos() {
     state.pos = Math.floor(Math.random() * cords.length)
   }
-  
-  function setImg(state){
-    img.style.bottom = `${state.y}em` 
-    img.style.right = `${state.x}em` 
+
+  function setImg(state) {
+    img.style.bottom = `${state.y}em`
+    img.style.right = `${state.x}em`
   }
-  
+
+  function setInstructor() {
+    let params = new URL(location.href).searchParams;
+    let instructor = {
+      name: params.get('instructor'),
+      photo: params.get('photo')
+    }
+    let name = document.getElementById('instructor-name');
+    let photo = document.getElementById('instructor-photo');
+    name.textContent = instructor.name || ''
+    photo.setAttribute('src', instructor.photo + '.jpg')
+  }
+
   setTimeout(transition, 5000)
-  
-  
+
+
 }())
